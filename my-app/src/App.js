df0675621ddc17824ba547b856e888e2452e0791
bonnkekimani <bonnke.kimani@student.moringaschool.com>
2023-05-02 19:28:55 +0300

    Bots Landing page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,6 +6,18 @@ import "./App.css"
 export default function App() {
   return (
     <div className="App">
+      <h1>Bots Page</h1>
+      <p>Click on the images and then enlist to move the images to the top div</p>
+      <p>Click on the x button to remove the element from the list</p>
+      <p>You can move the images from the top div to bottom div by following either of the 2 ways</p>
+      <li>Click on the 'Go back' button</li>
+      <li>Click on the image</li>
+      
+
+
+
+
+      
       <BotsPage />
     </div>
   )
